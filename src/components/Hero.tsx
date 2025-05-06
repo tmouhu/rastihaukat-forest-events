d701e26290742c1527558ec06bde3786351396e6
timmou <timo.mouhu@triona.fi>
2025-05-06 16:12:12 +0300

    chore: gh-pages

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,20 +6,17 @@ import rahaLogoNegTxt from "@/assets/raha-logo-neg-txt.svg";
 
 const Hero = () => {
   const { t } = useLocalization();
-  
+
   return (
     <div className="relative overflow-hidden bg-forest-dark">
       <div className="absolute inset-0 bg-topo-pattern opacity-20"></div>
       <div className="container mx-auto px-4 py-16 md:py-24 relative z-10">
         <div className="max-w-3xl mx-auto text-center text-white">
-          <div className="flex justify-center mb-4">
-          <img src={kuusiIcon}  alt="Kuusi Icon" className="h-10 w-10" />
+          <div className="flex justify-center mb-2">
+            <img src={rahaLogoNegTxt} alt="Raha logo" className="h-32 mx-auto" />
           </div>
           <h1 className="text-3xl md:text-5xl font-bold mb-4">{t("heroTitle")}</h1>
-          <div className="mb-6">
-            <p className="text-lg md:text-xl text-white/90">{t("heroDescription")}</p>
-            <img src={rahaLogoNegTxt} alt="Raha logo" className="h-40 mx-auto" />
-          </div>
+          <p className="mb-6 text-lg md:text-xl text-white/90">{t("heroDescription")}</p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <Button className="bg-brand-orange hover:bg-brand-orange/90 text-white" size="lg" asChild>
               <a href="#events">{t("showUpcomingEvents")}</a>
