107ee3a8c5b6df35064fd776ebc8ac471764166e
timmou <timo.mouhu@triona.fi>
2025-05-06 16:41:01 +0300

    chore: gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/archive/:year/:season" element={<SeasonArchive />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<Index />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
