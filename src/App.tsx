06fae2d1c1bce77d13303d19d9fe32dceae77a9d
timmou <timo.mouhu@triona.fi>
2025-05-06 16:32:26 +0300

    chore: gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LocalizationProvider } from "@/hooks/useLocalization";
 import Index from "./pages/Index";
 import SeasonArchive from "./pages/SeasonArchive";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -22,7 +21,6 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/archive/:year/:season" element={<SeasonArchive />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
